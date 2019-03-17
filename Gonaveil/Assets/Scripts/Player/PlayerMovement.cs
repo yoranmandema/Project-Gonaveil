eaa9c552b3232db121394ae96bc1dc19f5983c99
Blieper <yoranmandema@eml.cc>
2019-03-18 00:36:29 +0100

    deccelaration

--- Gonaveil/Assets/Scripts/Player/PlayerMovement.cs
+++ Gonaveil/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float maxVelocity = 5f;
     public float acceleration = 50f;
+    public float decceleration = 50f;
 
     private Vector3 velocity;
     private Rigidbody rb;
@@ -18,11 +19,20 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         var velocityY = rb.velocity.y;
+        var localVel = transform.InverseTransformDirection(rb.velocity);
+
+        var forwardAcceleration = -localVel.z * decceleration;
+        var sideAcceleration = -localVel.x * decceleration;
+
+        if (Input.GetAxis("Vertical") != 0) forwardAcceleration = Input.GetAxis("Vertical") * acceleration;
+        if (Input.GetAxis("Horizontal") != 0) sideAcceleration = Input.GetAxis("Horizontal") * acceleration;
 
         rb.velocity = 
             rb.velocity + 
-            transform.forward * Input.GetAxis("Vertical") + 
-            transform.right * Input.GetAxis("Horizontal");
+            (transform.forward * forwardAcceleration + 
+            transform.right * sideAcceleration) * Time.deltaTime;
+
+
 
         rb.velocity = Vector3.ClampMagnitude(Vector3.Scale(rb.velocity,new Vector3(1, 0, 1)), maxVelocity).SetY(velocityY);
     }
