050205d460d4abd89a8c5d36c1a9a47cd0403b38
Blieper <yoranmandema@eml.cc>
2019-03-18 00:28:32 +0100

    Basic rigidbody player movement

---
+++ Gonaveil/Assets/Scripts/Math/VectorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public static class VectorExtensions {
+    public static Vector3 SetX(this Vector3 vector, float v) {
+        return new Vector3(v, vector.y, vector.z);
+    }
+
+    public static Vector3 SetY(this Vector3 vector, float v) {
+        return new Vector3(vector.x, v, vector.z);
+    }
+
+    public static Vector3 SetZ(this Vector3 vector, float v) {
+        return new Vector3(vector.x, vector.y, v);
+    }
+}
---
+++ Gonaveil/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float maxVelocity = 5f;
+    public float acceleration = 50f;
+
+    private Vector3 velocity;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        var velocityY = rb.velocity.y;
+
+        rb.velocity = 
+            rb.velocity + 
+            transform.forward * Input.GetAxis("Vertical") + 
+            transform.right * Input.GetAxis("Horizontal");
+
+        rb.velocity = Vector3.ClampMagnitude(Vector3.Scale(rb.velocity,new Vector3(1, 0, 1)), maxVelocity).SetY(velocityY);
+    }
+}
