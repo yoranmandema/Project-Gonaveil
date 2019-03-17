da7d3ed8eb923f17229fdf524cf41af53983e608
Blieper <yoranmandema@eml.cc>
2019-03-18 00:11:26 +0100

    Very basic mouse movement

---
+++ Gonaveil/Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+
+    public float sensitivity = 1f;
+
+    void Update()
+    {
+        transform.parent.Rotate(new Vector3(0,Input.GetAxis("Mouse X"),0));
+        transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0, 0), Space.Self);
+    }
+}
