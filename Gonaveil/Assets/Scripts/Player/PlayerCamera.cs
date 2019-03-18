d74aa14a491e79f66ecba4bd5f1c8eca7e41f101
Galaxyzd <thomaswillyam@live.nl>
2019-03-18 11:35:08 +0100

    oops i forgot some shit
    minecraft steve lol

--- Gonaveil/Assets/Scripts/Player/PlayerCamera.cs
+++ Gonaveil/Assets/Scripts/Player/PlayerCamera.cs
@@ -16,9 +16,6 @@ public class PlayerCamera : MonoBehaviour {
         transform.parent.Rotate(new Vector3(0,Input.GetAxis("Mouse X"),0));
         mouseY -= Input.GetAxis("Mouse Y");
         mouseY = Mathf.Clamp(mouseY, -90, 90);
-
-        //transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0, 0), Space.Self);
-        print(transform.localEulerAngles);
         transform.localEulerAngles = new Vector3(mouseY, 0, 0);
     }
 }
