cbbfb59a43742c3cae7a8eaafaa07581e219793b
Blieper <yoranmandema@eml.cc>
2019-03-19 20:02:33 +0100

    Code cleanup + better velocity logic

--- Gonaveil/Assets/Scripts/Player/PlayerMovement.cs
+++ Gonaveil/Assets/Scripts/Player/PlayerMovement.cs
@@ -13,7 +13,7 @@ public class PlayerMovement : MonoBehaviour {
     public bool autoJump;
     public float jumpHeight = 1f;
     public float jumpLateralSpeedMultiplier = 1.1f;
-    public float fallMultiplierFloat = 1.5f;
+    public float fallSpeedMultiplier = 1.5f;
     public float fallMaxSpeedUp = 10f;
 
     public bool isOnWalkableGround;
@@ -22,18 +22,17 @@ public class PlayerMovement : MonoBehaviour {
 
     public LayerMask groundedLayerMask;
 
-    public Vector3 groundedNormal;
-    public Vector3 lastGroundedNormal;
-
     private CharacterController characterController;
     private Vector3 velocity;
+    private Vector3 desiredMovement;
     private Rigidbody rb;
     private float lateralJumpVelocity;
 
     private bool wantsJump;
     private bool canJump = true;
+    private bool wasGrounded;
 
-    void Start() {
+    private void Start() {
         rb = GetComponent<Rigidbody>();
         characterController = GetComponent<CharacterController>();
     }
@@ -46,41 +45,63 @@ public class PlayerMovement : MonoBehaviour {
         }
     }
 
+    private void CheckIfGrounded () {
+        wasGrounded = isGrounded;
+
+        isGrounded = Physics.CheckSphere(transform.position + Vector3.up * (characterController.radius - 0.2f), characterController.radius - 0.1f, groundedLayerMask, QueryTriggerInteraction.Ignore);
+    }
+
     void Update() {
-        isGrounded = Physics.CheckSphere(transform.position + Vector3.up * characterController.radius, characterController.radius + 0.1f, groundedLayerMask, QueryTriggerInteraction.Ignore);
+        CheckIfGrounded();
 
-        Debug.DrawLine(transform.position + Vector3.up * characterController.radius, transform.position + Vector3.up * characterController.radius - Vector3.up * (characterController.radius + 0.01f));
-
-        var desiredVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        var transformedVelocity = transform.TransformDirection(desiredVelocity);
+        desiredMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
         if (isGrounded) {
-            if (canJump) canJump = false;
+            if (!wasGrounded) canJump = true;
 
-            desiredVelocity.Normalize();
-
-            velocity += Vector3.ClampMagnitude(transformedVelocity * maxVelocity - velocity, acceleration * Time.deltaTime);
-
-            if (WantsJumpInput() && canJump) {
-                velocity = Vector3.up * Mathf.Sqrt(jumpHeight * 2f * Physics.gravity.magnitude) + Vector3.Scale(velocity, new Vector3(jumpLateralSpeedMultiplier, 0, jumpLateralSpeedMultiplier));
-
-                lateralJumpVelocity = Vector3.Scale(velocity, new Vector3(1,0,1)).magnitude;
-            }
+            GroundMovement();
         }
         else {
-            if (Vector3.Scale(velocity, new Vector3(1, 0, 1)).magnitude < lateralJumpVelocity) {
-                velocity += transformedVelocity * airAccelaration * Time.deltaTime;
-            }
-
-            velocity -= velocity * airDrag * Time.deltaTime;
-
-            if (velocity.y < 0 && velocity.y > -fallMaxSpeedUp) {
-                velocity += Vector3.up * Physics.gravity.y * (fallMultiplierFloat - 1) * Time.deltaTime;
-            }
-
-            velocity += Physics.gravity * Time.deltaTime;
+            AirMovement();
         }
 
         characterController.Move(velocity * Time.deltaTime);
+    }
+
+    private void GroundMovement () {
+        desiredMovement.Normalize();
+
+        var transformedMovement = transform.TransformDirection(desiredMovement);
+
+        velocity += Vector3.ClampMagnitude(transformedMovement * maxVelocity - velocity, acceleration * Time.deltaTime);
+
+        if (WantsJumpInput() && canJump) {
+            canJump = false;
+
+            velocity = Vector3.up * Mathf.Sqrt(jumpHeight * 2f * Physics.gravity.magnitude) + Vector3.Scale(velocity, new Vector3(jumpLateralSpeedMultiplier, 0, jumpLateralSpeedMultiplier));
+
+            lateralJumpVelocity = Vector3.Scale(velocity, new Vector3(1, 0, 1)).magnitude;
+        }
+    }
+
+    private void AirMovement () {
+
+        // Faster fall velocity.
+        if (Vector3.Scale(velocity, new Vector3(1, 0, 1)).magnitude < lateralJumpVelocity) {
+            var transformedMovement = transform.TransformDirection(desiredMovement);
+
+            velocity += transformedMovement * airAccelaration * Time.deltaTime;
+        }
+
+        // Air drag.
+        velocity -= velocity * airDrag * Time.deltaTime;
+
+        // Lateral air acceleration.
+        if (velocity.y < 0 && velocity.y > -fallMaxSpeedUp) {
+            velocity += Vector3.up * Physics.gravity.y * (fallSpeedMultiplier - 1) * Time.deltaTime;
+        }
+
+        // Gravity.
+        velocity += Physics.gravity * Time.deltaTime;
     }
 }
