f6faa8076f7f039d5090e28afb84a1ca988285f8
Galaxy <thomaswillyam@live.nl>
2019-03-20 15:37:42 +0100

    added extremely basic shooting mechanics

---
+++ Gonaveil/Assets/Scripts/Player/WeaponHolder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHolder : MonoBehaviour {
+    [SerializeField]
+    private Weapon currentWeapon;
+
+    private void Start() {
+        currentWeapon = GetComponentInChildren<Weapon>();
+    }
+}
