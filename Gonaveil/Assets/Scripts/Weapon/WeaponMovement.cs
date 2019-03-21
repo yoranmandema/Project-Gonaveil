f05e46aa3dddc3667bede41b104075ca233dfd23
Blieper <yoranmandema@eml.cc>
2019-03-21 20:44:28 +0100

    retract weapon when looking down

--- Gonaveil/Assets/Scripts/Weapon/WeaponMovement.cs
+++ Gonaveil/Assets/Scripts/Weapon/WeaponMovement.cs
@@ -17,13 +17,17 @@ public class WeaponMovement : MonoBehaviour
     public float crouchAngle = 75f;
     public float crouchEngageTime = 0.1f;
     public float crouchEngageSmoothing = 0.05f;
-    
+
+    public float lookDownRetraction = 0.25f;
+    public float lookDownSmoothing = 2f;
+
     public float jumpAmount = 0.25f;
 
     private float bobbingLerp = 1f;
     private float bobbingStep = 0f;
     private float crouchingLerp = 0f;
     private float crouchingSmoothedLerp = 0f;
+    private float lookDownLerp = 0f;
 
     private Vector3 standardPosition;
 
@@ -45,8 +49,8 @@ public class WeaponMovement : MonoBehaviour
 
         bobbingLerp = Mathf.Clamp(bobbingLerp + (isGrounded ? 1 : -1) * Time.deltaTime / bobbingEngageTime, 0, 1f);
         crouchingLerp = Mathf.Clamp(crouchingLerp + (playerMovement.isCrouching || playerMovement.isSliding ? 1 : -1) * Time.deltaTime / crouchEngageTime, 0, 1f);
-
         crouchingSmoothedLerp += (crouchingLerp - crouchingSmoothedLerp) * Time.deltaTime / crouchEngageSmoothing;
+        lookDownLerp += (Mathf.Clamp(transform.forward.y, -1, 0) - lookDownLerp) * Time.deltaTime / lookDownSmoothing;
 
         if (isGrounded) bobbingStep += velocityLerp + Time.deltaTime;
 
@@ -60,8 +64,10 @@ public class WeaponMovement : MonoBehaviour
 
         sideComponent += Vector3.right * -crouchingSmoothedLerp * 0.4f;
 
+        forwardComponent += Vector3.forward * lookDownLerp * lookDownRetraction;
+
         transform.localPosition = standardPosition + (
-            sideComponent +
+            sideComponent +     
             forwardComponent +
             upComponent
             ) * bobbingAmount;
