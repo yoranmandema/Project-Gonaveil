14eac2067503f425687ff0bd7b9cd030005e7964
Cathier <cathiercrowd@gmail.com>
2019-03-21 17:40:18 +0100

    Just me trying to fix shit

---
+++ Gonaveil/Assets/Scripts/Networking/NetworkPlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Networking;
+
+public class NetworkPlayerController : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
