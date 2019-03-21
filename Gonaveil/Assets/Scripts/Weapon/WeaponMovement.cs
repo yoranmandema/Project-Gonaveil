f34d047fa4a4aa61572904de290610a17d4b5428
Blieper <yoranmandema@eml.cc>
2019-03-21 20:38:07 +0100

    Angle gun when crouching

--- Gonaveil/Assets/Scripts/Weapon/WeaponMovement.cs
+++ Gonaveil/Assets/Scripts/Weapon/WeaponMovement.cs
@@ -6,18 +6,32 @@ public class WeaponMovement : MonoBehaviour
 {
     public PlayerMovement playerMovement;
 
+    public float velocityMultiplier = 0.05f;
+
     public float rotationSpeed = 0.5f;
     public float rotationAmount = 2.5f;
-    public float velocityMultiplier = 0.05f;
+
     public float bobbingAmount = 1f;
     public float bobbingEngageTime = 0.25f;
+
+    public float crouchAngle = 75f;
+    public float crouchEngageTime = 0.1f;
+    public float crouchEngageSmoothing = 0.05f;
     
+    public float jumpAmount = 0.25f;
+
     private float bobbingLerp = 1f;
     private float bobbingStep = 0f;
+    private float crouchingLerp = 0f;
+    private float crouchingSmoothedLerp = 0f;
+
     private Vector3 standardPosition;
 
     private float yaw;
     private float pitch;
+
+    private bool wasInAir;
+    private bool wasGrounded;
 
     void Start()
     {
@@ -30,6 +44,9 @@ public class WeaponMovement : MonoBehaviour
         var velocityLerp = playerMovement.velocity.magnitude * velocityMultiplier;
 
         bobbingLerp = Mathf.Clamp(bobbingLerp + (isGrounded ? 1 : -1) * Time.deltaTime / bobbingEngageTime, 0, 1f);
+        crouchingLerp = Mathf.Clamp(crouchingLerp + (playerMovement.isCrouching || playerMovement.isSliding ? 1 : -1) * Time.deltaTime / crouchEngageTime, 0, 1f);
+
+        crouchingSmoothedLerp += (crouchingLerp - crouchingSmoothedLerp) * Time.deltaTime / crouchEngageSmoothing;
 
         if (isGrounded) bobbingStep += velocityLerp + Time.deltaTime;
 
@@ -41,6 +58,8 @@ public class WeaponMovement : MonoBehaviour
         forwardComponent += Vector3.forward * Mathf.Sin(bobbingStep * 2f) * bobbingLerp * 0.25f * velocityLerp;
         upComponent += Vector3.up * Mathf.Sin(bobbingStep * 2f) * bobbingLerp * 0.25f * velocityLerp;
 
+        sideComponent += Vector3.right * -crouchingSmoothedLerp * 0.4f;
+
         transform.localPosition = standardPosition + (
             sideComponent +
             forwardComponent +
@@ -50,6 +69,13 @@ public class WeaponMovement : MonoBehaviour
         yaw += (Input.GetAxis("Mouse X") - yaw) * Time.deltaTime * rotationSpeed;
         pitch += (-Input.GetAxis("Mouse Y") - pitch) * Time.deltaTime * rotationSpeed;
 
-        transform.localRotation = Quaternion.Euler(pitch * rotationAmount, yaw * rotationAmount, 0);
+        //if (wasInAir != playerMovement.isInAir || wasGrounded != isGrounded) {
+        //    pitch += playerMovement.velocity.y * jumpAmount;
+        //}
+
+        transform.localRotation = Quaternion.Euler(pitch * rotationAmount, yaw * rotationAmount, crouchingSmoothedLerp * crouchAngle);
+
+        wasInAir = playerMovement.isInAir;
+        wasGrounded = isGrounded;
     }
 }
