616c3597f90e4d5b03270494a0dbf91551dd5c4f
Dirantai <pixl.nate@gmail.com>
2019-03-22 22:05:04 +0000

    better pickup!

--- Gonaveil/Assets/DroppedWeaponData.cs
+++ Gonaveil/Assets/DroppedWeaponData.cs
@@ -5,4 +5,17 @@ using UnityEngine;
 public class DroppedWeaponData : MonoBehaviour
 {
     public WeaponParameters weaponParameters;
+    private CapsuleCollider pickupTrigger;
+
+    public void Intangible(CapsuleCollider playerCollider)
+    {
+        pickupTrigger = playerCollider;
+        Physics.IgnoreCollision(GetComponent<BoxCollider>(), pickupTrigger, true);
+        Invoke("Tangible", 1);
+    }
+
+    void Tangible()
+    {
+        Physics.IgnoreCollision(GetComponent<BoxCollider>(), pickupTrigger, false); 
+    }
 }
