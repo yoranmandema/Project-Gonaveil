a1749c5ec66db0814a9e182b29b09bcf34ac3942
Cathier <cathiercrowd@gmail.com>
2019-03-22 20:03:44 +0100

    Input system example (to discuss)

---
+++ Gonaveil/Assets/Scripts/InputSystem/Key.cs
@@ -0,0 +1,20 @@
+﻿namespace KeyInput
+{
+    enum Key
+    {
+        Jump,
+        Crouch,
+        NextWeapon,
+        PrevWeapon,
+        Drop,
+        Use,
+    }
+
+    enum Axis
+    {
+        MovementAxisX,
+        MovementAxisY,
+        LookAxisX,
+        LookAxisY,
+    }
+}
