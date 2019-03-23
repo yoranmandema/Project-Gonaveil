4f58caf83aa9e39429a110646f09497addca8a1f
Dirantai <pixl.nate@gmail.com>
2019-03-23 01:33:50 +0000

    changed projectiles so they spawn differently

--- Gonaveil/Assets/Scripts/Bullet.cs
+++ Gonaveil/Assets/Scripts/Bullet.cs
@@ -9,20 +9,27 @@ public class Bullet : MonoBehaviour
     public float decayTime;
     public GameObject impactObject;
     public Rigidbody bulletRigid;
+    public Transform barrel;
+    public Transform effect;
+    Vector3 offset;
 
     void Start()
     {
+        effect.position = barrel.position;
+        offset = effect.localPosition;
         Destroy(gameObject, decayTime);
     }
 
     // Update is called once per frame
     void Update()
     {
+        effect.localPosition = Vector3.Lerp(effect.localPosition, Vector3.zero, 10f * Time.deltaTime);
         bulletRigid.velocity = transform.forward * velocity;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        effect.localPosition = Vector3.zero;
         Instantiate(impactObject, collision.contacts[0].point, new Quaternion(0, 0, 0, 0), collision.transform);
         Destroy(gameObject);
     }
