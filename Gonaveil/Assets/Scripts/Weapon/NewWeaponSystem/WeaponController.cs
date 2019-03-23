cf3e122f7956a626ae34f4d9953d1f7ae94e6203
Blieper <yoranmandema@eml.cc>
2019-03-23 23:14:36 +0100

    WeaponInventory class

--- Gonaveil/Assets/Scripts/Weapon/NewWeaponSystem/WeaponController.cs
+++ Gonaveil/Assets/Scripts/Weapon/NewWeaponSystem/WeaponController.cs
@@ -14,6 +14,7 @@ public class WeaponController : MonoBehaviour
     private GameObject worldModel;
     private Transform barrel;
     private WeaponModelData modelData;
+    private InventorySystem inventorySystem;
 
     public void SetParameters(WeaponParameters parameters) {
         if (viewModel != null) Destroy(viewModel);
@@ -21,24 +22,18 @@ public class WeaponController : MonoBehaviour
 
         weaponParameters = parameters;
  
-        //creates new view model and world model.
         viewModel = Instantiate(weaponParameters.viewModel, transform, false);
         worldModel = Instantiate(weaponParameters.worldModel, handBone, false);
-        //apply world model transformations
-        worldModel.transform.localEulerAngles = new Vector3(-90, 90, 0);
 
-        //get the model data for the barrel.
         modelData = viewModel.GetComponent<WeaponModelData>();
         barrel = modelData.barrel;
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        inventorySystem = GetComponent<InventorySystem>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
