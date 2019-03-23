46bd9a42ff671dc5c9a8b1c048faaa1daab34e3d
Blieper <yoranmandema@eml.cc>
2019-03-23 14:04:20 +0100

    Put weaponName in parameters instead of weapon stats

--- Gonaveil/Assets/Scripts/InventorySystem.cs
+++ Gonaveil/Assets/Scripts/InventorySystem.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventorySystem : MonoBehaviour
-{
+public class InventorySystem : MonoBehaviour {
     public WeaponParameters[] allWeapons;
     public Weapon weaponMaster;
     public int selectedWeaponID;
@@ -17,14 +16,12 @@ public class InventorySystem : MonoBehaviour
     private WeaponParameters current;
     private bool disabled;
 
-    void Start()
-    {
+    void Start() {
         //weaponMovement = GetComponent<WeaponMovement>();
         lastSelectedWeaponID = 1;
     }
 
-    void Update()
-    {
+    void Update() {
         CheckInventory();
         if (Input.GetAxis("Mouse ScrollWheel") > 0) {
             ++selectedWeaponID;
@@ -32,105 +29,92 @@ public class InventorySystem : MonoBehaviour
         else if (Input.GetAxis("Mouse ScrollWheel") < 0) {
             --selectedWeaponID;
         }
-        if (selectedWeaponID > 1)
-        {
+        if (selectedWeaponID > 1) {
             selectedWeaponID = 0;
-        } else if (selectedWeaponID < 0)
-        {
+        }
+        else if (selectedWeaponID < 0) {
             selectedWeaponID = 1;
         }
-        if (allWeapons[selectedWeaponID] == null)
-        {
-            if (selectedWeaponID == 1)
-            {
+        if (allWeapons[selectedWeaponID] == null) {
+            if (selectedWeaponID == 1) {
                 selectedWeaponID = 0;
             }
-            else if (selectedWeaponID == 0)
-            {
+            else if (selectedWeaponID == 0) {
                 selectedWeaponID = 1;
             }
         }
-        if (lastSelectedWeaponID != selectedWeaponID && !disabled)
-        {
+        if (lastSelectedWeaponID != selectedWeaponID && !disabled) {
             SetWeapon();
         }
-        if (Input.GetButtonDown("Drop Weapon") && !disabled)
-        {
+        if (Input.GetButtonDown("Drop Weapon") && !disabled) {
             DropWeapon();
         }
     }
 
-    void CheckInventory()
-    {
-        if(allWeapons[0] == null && allWeapons[1] == null)
-        {
+    void CheckInventory() {
+        if (allWeapons[0] == null && allWeapons[1] == null) {
             currentWeaponText.text = "None";
             weaponMaster.Disarm();
             disabled = true;
         }
-        else
-        {
+        else {
             weaponMaster.Rearm();
             disabled = false;
         }
     }
 
-    void DropWeapon()
-    {
+    void DropWeapon() {
         holsteredWeaponText.text = "None";
+
         GameObject dropItem = Instantiate(currentDropObject, transform.position + transform.up, transform.rotation) as GameObject;
         dropItem.GetComponent<DroppedWeaponData>().Intangible(GetComponentInChildren<CapsuleCollider>());
         dropItem.GetComponent<DroppedWeaponData>().weaponParameters = allWeapons[selectedWeaponID];
-        allWeapons[selectedWeaponID] = null;
         dropItem.transform.Rotate(0, 90, 0);
-        var throwVector = new Vector3();
-        throwVector = transform.forward + Vector3.up;
+
+        allWeapons[selectedWeaponID] = null;
+
+        var throwVector = transform.forward + Vector3.up;
         dropItem.GetComponent<Rigidbody>().AddForce(throwVector * 100);
     }
 
-    private void OnTriggerStay(Collider collision)
-    {
-        if (collision.tag == "ItemWeapon")
-        {
+    private void OnTriggerStay(Collider collision) {
+        if (collision.tag == "ItemWeapon") {
             WeaponParameters droppedParameters = collision.GetComponentInChildren<DroppedWeaponData>().weaponParameters;
-            CheckInventory(0, 1, collision.gameObject, droppedParameters, currentWeaponText);
-            CheckInventory(1, 0, collision.gameObject, droppedParameters, holsteredWeaponText);
+            //CheckInventory(0, 1, collision.gameObject, droppedParameters, currentWeaponText);
+            //CheckInventory(1, 0, collision.gameObject, droppedParameters, holsteredWeaponText);
+
+
         }
     }
 
-    void CheckInventory(int PrimaryID, int SecondaryID, GameObject item, WeaponParameters droppedParameters, UnityEngine.UI.Text TextDisplay)
-    {
-        if (allWeapons[PrimaryID] == null)
-        {
-            try
-            {
-                if (allWeapons[SecondaryID].name != droppedParameters.name)
-                {
-                    PickupItem(PrimaryID, SecondaryID, item, droppedParameters, TextDisplay);
-                }
-            }
-            catch
-            {
-                PickupItem(PrimaryID, SecondaryID, item, droppedParameters, TextDisplay);
-            }
-        }
+    void TryToPickupWeapon(GameObject item, WeaponParameters droppedParameters, UnityEngine.UI.Text TextDisplay) {
+        //if (allWeapons[PrimaryID] == null) {
+        //    try {
+        //        if (allWeapons[SecondaryID].name != droppedParameters.name) {
+        //            PickupItem(PrimaryID, SecondaryID, item, droppedParameters, TextDisplay);
+        //        }
+        //    }
+        //    catch {
+        //        PickupItem(PrimaryID, SecondaryID, item, droppedParameters, TextDisplay);
+        //    }
+        //}
+
+        //if (allWeapons[0] == null) {
+
+        //}
+
+        //PickupItem(PrimaryID, SecondaryID, item, droppedParameters, TextDisplay);
     }
 
-    void PickupItem(int InventorySlot, int SecondarySlot, GameObject item, WeaponParameters droppedParameters, UnityEngine.UI.Text TextDisplay)
-    {
+    void PickupItem(int InventorySlot, int SecondarySlot, GameObject item, WeaponParameters droppedParameters) {
         allWeapons[InventorySlot] = droppedParameters;
-        TextDisplay.text = allWeapons[InventorySlot].weaponStats.WeaponName;
+
         lastSelectedWeaponID = SecondarySlot;
+
         Destroy(item);
     }
 
     void SetWeapon() {
-        //if (lastSelectedWeaponID > -1) allWeapons[lastSelectedWeaponID].weaponStats.modelObject.SetActive(false);
-        currentWeaponText.text = allWeapons[selectedWeaponID].weaponStats.WeaponName;
-        if (lastSelectedWeaponID != -1 && allWeapons[lastSelectedWeaponID] != null)
-        {
-            holsteredWeaponText.text = allWeapons[lastSelectedWeaponID].weaponStats.WeaponName;
-        }
         current = allWeapons[selectedWeaponID];
 
         weaponMaster.SetParameters(current);
