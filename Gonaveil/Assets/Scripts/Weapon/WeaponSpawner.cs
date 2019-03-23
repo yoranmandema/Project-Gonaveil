5c4c48985b88ca189ae8c0fa4dcb1cf8de3b51d3
Dirantai <pixl.nate@gmail.com>
2019-03-23 21:17:12 +0000

    added weapon spawner

---
+++ Gonaveil/Assets/Scripts/Weapon/WeaponSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSpawner : MonoBehaviour
+{
+    public GameObject weaponPrefab;
+    public float spawnDelay;
+    public Transform spawnPosition;
+    private GameObject spawnedWeapon;
+    private float spawnTimer;
+    public bool spawnOnStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (spawnOnStart)
+        {
+            SpawnWeapon();
+        }
+    }
+
+    void SpawnWeapon()
+    {
+        spawnTimer = spawnDelay;
+        spawnedWeapon = Instantiate(weaponPrefab, spawnPosition.position, transform.rotation) as GameObject;
+        spawnedWeapon.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spawnedWeapon == null)
+        {
+            spawnTimer -= Time.deltaTime;
+            if(spawnTimer <= 0)
+            {
+                SpawnWeapon();
+            }
+        }
+    }
+}
