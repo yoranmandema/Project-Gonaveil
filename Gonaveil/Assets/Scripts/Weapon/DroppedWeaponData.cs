a6f5cf05c7e9610abf4d604a40c59cf98f833b75
Dirantai <pixl.nate@gmail.com>
2019-03-23 14:23:21 +0000

    moar comments

--- Gonaveil/Assets/Scripts/Weapon/DroppedWeaponData.cs
+++ Gonaveil/Assets/Scripts/Weapon/DroppedWeaponData.cs
@@ -9,21 +9,26 @@ public class DroppedWeaponData : MonoBehaviour
     public int currentAmmoPool;
     private CapsuleCollider pickupTrigger;
 
+    //apply the ammo in the inventory to the dropped weapon.
     public void SetDroppedAmmo(int magazine, int ammoPool)
     {
         currentMagazineCapacity = magazine;
         currentAmmoPool = ammoPool;
     }
-
+    
+    //used to enusre the weapon cannot be picked up for a short time.
     public void Intangible(CapsuleCollider playerCollider)
     {
+        //get the collider and ignore collision
         pickupTrigger = playerCollider;
         Physics.IgnoreCollision(GetComponent<BoxCollider>(), pickupTrigger, true);
+        //invoke another class to allow the weapon to be picked up again
         Invoke("Tangible", 0.5f);
     }
 
     void Tangible()
     {
+        //stop ignoring
         Physics.IgnoreCollision(GetComponent<BoxCollider>(), pickupTrigger, false); 
     }
 }
