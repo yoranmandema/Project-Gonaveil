566d3dc0ea91c6d11aff64877b1bacc7738f4231
Dirantai <pixl.nate@gmail.com>
2019-03-23 14:25:03 +0000

    comments on the bullet

--- Gonaveil/Assets/Scripts/Bullet.cs
+++ Gonaveil/Assets/Scripts/Bullet.cs
@@ -11,26 +11,31 @@ public class Bullet : MonoBehaviour
     public Rigidbody bulletRigid;
     public Transform barrel;
     public Transform effect;
-    Vector3 offset;
 
     void Start()
     {
+        //sets gets the bullet effect position to the barrel
         effect.position = barrel.position;
-        offset = effect.localPosition;
+        //destroy the gameobject after a certain time.
         Destroy(gameObject, decayTime);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //move the effect slowly to the centre of the actual bullet.
         effect.localPosition = Vector3.Lerp(effect.localPosition, Vector3.zero, 10f * Time.deltaTime);
+        //apply velocity
         bulletRigid.velocity = transform.forward * velocity;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        //force bullet effect to centre
         effect.localPosition = Vector3.zero;
+        //create impact
         Instantiate(impactObject, collision.contacts[0].point, new Quaternion(0, 0, 0, 0), collision.transform);
+        //destroy bullet
         Destroy(gameObject);
     }
 }
