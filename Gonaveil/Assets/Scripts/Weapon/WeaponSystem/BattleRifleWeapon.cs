7590b5a80964c14e81e462930f1a498e255da479
Blieper <yoranmandema@eml.cc>
2019-03-30 22:12:13 +0100

    base weapon system

---
+++ Gonaveil/Assets/Scripts/Weapon/WeaponSystem/BattleRifleWeapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleRifleWeapon : WeaponSystem {
+
+    private void Start () {
+        Disable();
+    }
+
+    public override void OnStartPrimary() {
+        base.OnStartPrimary();
+
+        print("Fired primary");
+    }
+
+    public override void OnStartSecondary() {
+        base.OnStartSecondary();
+
+        print("Fired secondary");
+    }
+}
---
+++ Gonaveil/Assets/Scripts/Weapon/WeaponSystem/WeaponSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSystem : MonoBehaviour {
+    public WeaponMovementProfile weaponMovementProfile;
+    public GameObject worldModel;
+    public WeaponMovement weaponMovement;
+
+    public bool isFiringPrimary;
+    public bool isFiringSecondary;
+
+    public void Enable () {
+        weaponMovement.profile = weaponMovementProfile;
+    }
+
+    public void Disable() {
+        isFiringPrimary = isFiringSecondary = false;
+    }
+
+    void Update() {
+        if (InputManager.GetButtonDown("Fire1") && !isFiringPrimary) {
+            OnStartPrimary();
+
+            isFiringPrimary = true;
+        } else if (InputManager.GetButtonUp("Fire1") && isFiringPrimary) {
+            OnEndPrimary();
+
+            isFiringPrimary = false;
+        }
+
+        if (InputManager.GetButtonDown("Fire2") && !isFiringSecondary) {
+            OnStartSecondary();
+
+            isFiringSecondary = true;
+        }
+        else if (InputManager.GetButtonUp("Fire2") && isFiringSecondary) {
+            OnEndSecondary();
+
+            isFiringSecondary = false;
+        }
+
+        if (InputManager.GetButtonDown("Reload")) {
+            OnReload();
+        }
+    }
+
+    public virtual void OnReload () { }
+
+    public virtual void OnStartPrimary() { }
+    public virtual void OnEndPrimary() { }
+
+    public virtual void OnStartSecondary() { }
+    public virtual void OnEndSecondary() { }
+}
