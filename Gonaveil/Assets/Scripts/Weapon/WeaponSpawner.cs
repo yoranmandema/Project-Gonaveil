65d52d33852b26baea22540bf47b550b40afafda
Blieper <yoranmandema@eml.cc>
2019-04-15 00:21:43 +0200

    spawn platforms spawn new weapon pickups

--- Gonaveil/Assets/Scripts/Weapon/WeaponSpawner.cs
+++ Gonaveil/Assets/Scripts/Weapon/WeaponSpawner.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponSpawner : MonoBehaviour
-{
+public class WeaponSpawner : MonoBehaviour {
     public GameObject weaponPrefab;
     public float spawnDelay;
     public Transform spawnPosition;
@@ -12,35 +11,24 @@ public class WeaponSpawner : MonoBehaviour
     public bool spawnOnStart;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        if (spawnOnStart)
-        {
+    void Start() {
+        if (spawnOnStart) {
             SpawnWeapon();
         }
     }
 
-    void SpawnWeapon()
-    {
+    void SpawnWeapon() {
         spawnTimer = spawnDelay;
         spawnedWeapon = Instantiate(weaponPrefab, spawnPosition.position, transform.rotation) as GameObject;
-        spawnedWeapon.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(spawnedWeapon == null)
-        {
+    void Update() {
+        if (spawnedWeapon == null) {
             spawnTimer -= Time.deltaTime;
-            if(spawnTimer <= 0)
-            {
+            if (spawnTimer <= 0) {
                 SpawnWeapon();
             }
-        }
-        else
-        {
-            spawnedWeapon.transform.Rotate(0, 50 * Time.deltaTime, 0);
         }
     }
 }
