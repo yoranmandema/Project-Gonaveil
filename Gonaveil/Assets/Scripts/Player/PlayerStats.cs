b85458b0f7fb3a2716560b5870e3a6fd7704f1d0
Blieper <yoranmandema@eml.cc>
2019-04-27 21:03:26 +0200

    respawn if health is 0

--- Gonaveil/Assets/Scripts/Player/PlayerStats.cs
+++ Gonaveil/Assets/Scripts/Player/PlayerStats.cs
@@ -9,30 +9,46 @@ public class PlayerStats : MonoBehaviour {
     public float respawnTime = 3f;
     public int health = 100;
 
+    public bool isRespawning = false;
+
     private PlayerMovement playerMovement;
 
     private void Start () {
         playerMovement = GetComponent<PlayerMovement>();
     }
 
+    private void Update () {
+        if (health == 0 && !isRespawning) {
+            Die();
+        }
+    }
+
     public void DealDamage (int amount) {
         health = Mathf.Max(0, health - amount);
 
-        if (health == 0) {
+        if (health == 0 && !isRespawning) {
             Die();
         }
     }
 
     public void Die () {
         playerMovement.allowInput = false;
+        isRespawning = true;
 
         StartCoroutine(RespawnTimer());
     }
 
     public void Spawn () {
         health = 100;
+        isRespawning = false;
 
         playerMovement.allowInput = true;
+
+        var spawns = GameObject.FindGameObjectsWithTag("PlayerSpawn");
+
+        var spawn = spawns[Random.Range(0, spawns.Length)];
+
+        transform.position = spawn.transform.position;
     }
 
     private IEnumerator RespawnTimer () {
