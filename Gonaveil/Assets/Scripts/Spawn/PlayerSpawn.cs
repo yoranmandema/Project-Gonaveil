0880c59ac56766dcbee9ee2b3b90522ad5140d03
Blieper <yoranmandema@eml.cc>
2019-04-28 00:48:39 +0200

    more map updates

--- Gonaveil/Assets/Scripts/Spawn/PlayerSpawn.cs
+++ Gonaveil/Assets/Scripts/Spawn/PlayerSpawn.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSpawn : MonoBehaviour
-{
+public class PlayerSpawn : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }
